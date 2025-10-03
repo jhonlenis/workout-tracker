b03d7c88938394318a51790a66918d492907acf3
jhonlenis <jhonalexander0606@gmail.com>
2025-10-02 23:43:41 -0500

    se hace el src y la app inicial

---
+++ src/app.js
@@ -0,0 +1,24 @@
+const express = require("express"); // Import express
+const app = express(); // Create an instance of express
+const { port } = require('./config/env'); // Import the port from the env file
+
+// Importar rutas
+const routes = require('./routes');
+
+// Middleware para parsear JSON
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get("/", (req, res) => {
+  res.send("Hola mi server en Express");
+});
+
+// Configurar rutas con prefijo /api
+app.use('/api', routes);
+
+// Inicio del servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
+

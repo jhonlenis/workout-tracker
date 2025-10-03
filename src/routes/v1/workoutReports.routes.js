b36fa167a2c28662cecb665d5ebcda5f2123026d
jhonlenis <jhonalexander0606@gmail.com>
2025-10-03 00:24:29 -0500

    se hace cambios

--- src/routes/v1/workoutReports.routes.js
+++ src/routes/v1/workoutReports.routes.js
@@ -2,4 +2,11 @@
 const express = require("express");
 const router = express.Router();
 const {
- 
+  getWorkoutReports,
+  getWorkoutReportById,
+  createWorkoutReport,
+  updateWorkoutReport,
+  deleteWorkoutReport
+} = require("../../controllers/workoutReports.controller");
+
+

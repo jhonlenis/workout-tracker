304f3bac46ad839b2f2266081fd17bcd897218ed
jhonlenis <jhonalexander0606@gmail.com>
2025-10-03 00:23:07 -0500

    se empieza a hacer el codigo

--- src/routes/v1/workoutReports.routes.js
+++ src/routes/v1/workoutReports.routes.js
@@ -2,18 +2,4 @@
 const express = require("express");
 const router = express.Router();
 const {
-  getWorkoutReports,
-  getWorkoutReportById,
-  createWorkoutReport,
-  updateWorkoutReport,
-  deleteWorkoutReport
-} = require("../../controllers/workoutReports.controller");
-
-// Rutas de workoutReports
-router.get("/", getWorkoutReports);
-router.get("/:id", getWorkoutReportById);
-router.post("/", createWorkoutReport);
-router.put("/:id", updateWorkoutReport);
-router.delete("/:id", deleteWorkoutReport);
-
-module.exports = router;
+ 

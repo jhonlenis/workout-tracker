32162a73fa837b57fac3fae15a1c98ac31177b34
jhonlenis <jhonalexander0606@gmail.com>
2025-10-03 00:17:33 -0500

    se empieza a hacer la constante

--- src/routes/v1/workoutPlans.routes.js
+++ src/routes/v1/workoutPlans.routes.js
@@ -1,4 +1,12 @@
 const express = require("express");
 const router = express.Router();
 
+const {
+  getWorkoutPlans,
+  getWorkoutPlanById,
+  createWorkoutPlan,
+  updateWorkoutPlan,
+  deleteWorkoutPlan
+} = require("../../controllers/workoutPlans.controller");
+
 
